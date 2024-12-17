3a1e526e997d45c93b0c0e4b00c7e347d89f88ab
Rassemaribi <Racem.Aribi@iit.ens.tn>
2024-12-17 23:33:44 +0100

    navbar

--- src/main/java/tn/iit/controller/ClientsController.java
+++ src/main/java/tn/iit/controller/ClientsController.java
@@ -13,27 +13,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import jakarta.websocket.server.PathParam;
 import lombok.AllArgsConstructor;
-import tn.iit.dto.StudentDto;
-import tn.iit.service.StudentService;
+import tn.iit.dto.ClientDto;
+import tn.iit.service.ClientService;
 
 @AllArgsConstructor
 @Controller
-@RequestMapping("/students")
+@RequestMapping("/clients")
 public class StudentsController {
-	private final StudentService service;
+	private final ClientService service;
 
 	@GetMapping({ "/", "/all", "/index" })
 	public String findall(Model model) {
 		model.addAttribute("students", service.findAll());
-		return "students";
+		return "clients";
 	}
 
 	@PostMapping("/save")
 	public String save(@RequestParam("id") int id, @RequestParam("firstName") String firstName,
 			@RequestParam("lastName") String lastName) {
-		StudentDto studentDto = new StudentDto(id, firstName, lastName);
+		ClientDto studentDto = new ClientDto(id, firstName, lastName);
 		service.save(studentDto);
 		return "redirect:/students/";
 
@@ -46,23 +45,23 @@ public class StudentsController {
 	}
 
 	@PostMapping("/search")
-	public ResponseEntity<List<StudentDto>> searchStudents(@RequestParam String key) {
-		List<StudentDto> students = service.findByName(key);
+	public ResponseEntity<List<ClientDto>> searchStudents(@RequestParam String key) {
+		List<ClientDto> students = service.findByName(key);
 		return ResponseEntity.ok(students);
 	}
 
 	@PostMapping("/edit")
 	public String edit(@RequestParam("id") int id, Model model) {
 
-		Optional<StudentDto> studentDtoOpt = service.findByid(id);
+		Optional<ClientDto> studentDtoOpt = service.findByid(id);
 		if (studentDtoOpt.isPresent()) {
 			model.addAttribute("student", studentDtoOpt.get());
 		}
-		return "students-edit";
+		return "clients-edit";
 
 	}
 	@PostMapping("/update")
-	public String update(@ModelAttribute StudentDto studentDto) {
+	public String update(@ModelAttribute ClientDto studentDto) {
 		service.update(studentDto);
 		return "redirect:/students/";
 	}
